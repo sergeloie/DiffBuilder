715c127784be47f9961d9f24d2feb71c04e8c459
serge loie <serge.loie@yandex.ru>
2023-08-16 17:57:16 +0500

    инициализация проекта шаг 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+// some exports omitted for the sake of brevity
+
+@Command(name = "ASCIIArt", version = "ASCIIArt 1.0", mixinStandardHelpOptions = true)
+public class App implements Runnable {
+
+    @Option(names = { "-s", "--font-size" }, description = "Font size")
+    int fontSize = 19;
+
+    @Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli",
+            description = "Words to be translated into ASCII art.")
+    private String[] words = { "Hello,", "picocli" };
+
+    @Override
+    public void run() {
+        // The business logic of the command goes here...
+        // In this case, code for generation of ASCII art graphics
+        // (omitted for the sake of brevity).
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
