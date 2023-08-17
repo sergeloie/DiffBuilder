03010fccad73bd854aa21e4f7530dc212706d249
serge loie <serge.loie@yandex.ru>
2023-08-17 14:24:13 +0500

    считывание файлов из параметров командной строки

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,10 +6,16 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 // some exports omitted for the sake of brevity
 
 @Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true)
-public class App implements Runnable {
+public class App implements Callable {
 
     @Parameters(paramLabel = "filepath1", description = "path to first file")
     File file1;
@@ -21,13 +27,35 @@ public class App implements Runnable {
     private String format = "stylish";
 
     @Override
-    public void run() {
+    public String call() throws IOException {
+
+        System.out.println(file1.toPath());
+        System.out.println(file2.toPath());
+
+        System.out.println(file1.getAbsolutePath());
+        System.out.println(file2.getAbsolutePath());
+
+        Path path1 = Paths.get(file1.getAbsolutePath());
+        Path path2 = Paths.get(file2.getAbsolutePath());
+
+        String contentOfJSONFile1 = Files.readString(path1);
+        String contentOfJSONFile2 = Files.readString(path2);
+
+        System.out.println(contentOfJSONFile1);
+        System.out.println(contentOfJSONFile2);
         // The business logic of the command goes here...
         // In this case, code for generation of ASCII art graphics
         // (omitted for the sake of brevity).
+        return new String();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+//        Path pathToJSONFile1 = Paths.get(file1).toAbsolutePath().normalize();
+//        Path pathToJSONFile2 = Paths.get(file2).toAbsolutePath().normalize();
+
+
+
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
