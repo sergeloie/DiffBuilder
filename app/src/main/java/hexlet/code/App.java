2cca3d473a510bd560ee850c7ef19b0e0bef45e2
serge loie <serge.loie@yandex.ru>
2023-08-17 12:33:08 +0500

    инициализация проекта шаг 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,17 +4,21 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
+import java.io.File;
 // some exports omitted for the sake of brevity
 
-@Command(name = "ASCIIArt", version = "ASCIIArt 1.0", mixinStandardHelpOptions = true)
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true)
 public class App implements Runnable {
 
-    @Option(names = { "-s", "--font-size" }, description = "Font size")
-    int fontSize = 19;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    File file1;
 
-    @Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli",
-            description = "Words to be translated into ASCII art.")
-    private String[] words = { "Hello,", "picocli" };
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    File file2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
 
     @Override
     public void run() {
