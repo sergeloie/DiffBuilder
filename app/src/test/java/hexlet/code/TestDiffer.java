f77f3a1702d4e1383f56e2862c18e91dd46c91f1
serge loie <serge.loie@yandex.ru>
2023-08-18 14:05:33 +0500

    реализация шага 6 исправление ошибок линтера

--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -18,26 +18,29 @@ public class TestDiffer {
 
     @Test
     public void testDifferJSON() throws JsonProcessingException {
-        String jsonString1 = "{\n"
-                + "  \"host\": \"hexlet.io\",\n"
-                + "  \"timeout\": 50,\n"
-                + "  \"proxy\": \"123.234.53.22\",\n"
-                + "  \"follow\": false\n"
-                + "}";
+        String jsonString1 = """
+                {
+                  "host": "hexlet.io",
+                  "timeout": 50,
+                  "proxy": "123.234.53.22",
+                  "follow": false
+                }""";
 
-        String jsonString2 = "{\n"
-                + "  \"timeout\": 20,\n"
-                + "  \"verbose\": true,\n"
-                + "  \"host\": \"hexlet.io\"\n"
-                + "}";
-        String expected = "{\n"
-                + "- follow: false\n"
-                + "  host: hexlet.io\n"
-                + "- proxy: 123.234.53.22\n"
-                + "- timeout: 50\n"
-                + "+ timeout: 20\n"
-                + "+ verbose: true\n"
-                + "}";
+        String jsonString2 = """
+                {
+                  "timeout": 20,
+                  "verbose": true,
+                  "host": "hexlet.io"
+                }""";
+        String expected = """
+                {
+                - follow: false
+                  host: hexlet.io
+                - proxy: 123.234.53.22
+                - timeout: 50
+                + timeout: 20
+                + verbose: true
+                }""";
         String actual = Differ.generate(jsonString1, jsonString2);
         assertEquals(expected, actual);
     }
