b835d26374d6655e4e3762e5a19b6ad0183a722a
serge loie <serge.loie@yandex.ru>
2023-08-18 11:42:32 +0500

    реализация шага 5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,13 +6,10 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
-// some exports omitted for the sake of brevity
 
 @Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true)
 public class App implements Callable {
@@ -29,29 +26,17 @@ public class App implements Callable {
     @Override
     public String call() throws Exception {
 
-//        System.out.println(file1.toPath());
-//        System.out.println(file2.toPath());
-//
-//        System.out.println(file1.getAbsolutePath());
-//        System.out.println(file2.getAbsolutePath());
-
         Path path1 = Paths.get(file1.getAbsolutePath());
         Path path2 = Paths.get(file2.getAbsolutePath());
 
         String contentOfJSONFile1 = Files.readString(path1);
         String contentOfJSONFile2 = Files.readString(path2);
 
-//        System.out.println(contentOfJSONFile1);
-//        System.out.println(contentOfJSONFile2);
-
-        Differ.generate(contentOfJSONFile1, contentOfJSONFile2);
-        // The business logic of the command goes here...
-        // In this case, code for generation of ASCII art graphics
-        // (omitted for the sake of brevity).
-        return "";
+        System.out.println(Differ.generate(contentOfJSONFile1, contentOfJSONFile2));
+        return Differ.generate(contentOfJSONFile1, contentOfJSONFile2);
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,45 +4,71 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.SortedMap;
-import java.util.LinkedHashMap;
-import java.util.TreeMap;
+import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Differ {
 
     public static String generate(String json1, String json2) throws JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> jsonMap1 = objectMapper.readValue(json1, new TypeReference<>(){});
-        Map<String, Object> jsonMap2 = objectMapper.readValue(json2, new TypeReference<>(){});
+        Map<String, Object> jsonMap1 = objectMapper.readValue(json1, new TypeReference<>() {
+        });
+        Map<String, Object> jsonMap2 = objectMapper.readValue(json2, new TypeReference<>() {
+        });
 
-        Map<String, Object> sortedJSON1 = new TreeMap<>(jsonMap1);
-        Map<String, Object> sortedJSON2 = new TreeMap<>(jsonMap2);
+        Set<String> controlSet = jsonMap1.keySet().stream().collect(Collectors.toSet());
+        controlSet.addAll(jsonMap2.keySet().stream().collect(Collectors.toSet()));
+        List<String> sortedList = new ArrayList<>(controlSet);
+        Collections.sort(sortedList);
 
+        StringBuilder stylishOutput = new StringBuilder();
+        stylishOutput.append("{\n");
 
+        for (String key : sortedList) {
+            if (jsonMap1.containsKey(key) && !jsonMap2.containsKey(key)) {
+                stylishOutput.append("- ");
+                stylishOutput.append(key);
+                stylishOutput.append(": ");
+                stylishOutput.append(jsonMap1.get(key));
+                stylishOutput.append("\n");
+                continue;
+            }
+            if (!jsonMap1.containsKey(key) && jsonMap2.containsKey(key)) {
+                stylishOutput.append("+ ");
+                stylishOutput.append(key);
+                stylishOutput.append(": ");
+                stylishOutput.append(jsonMap2.get(key));
+                stylishOutput.append("\n");
+                continue;
+            }
+            if (jsonMap2.containsKey(key) && jsonMap2.containsKey(key)) {
+                if (jsonMap1.get(key).equals(jsonMap2.get(key))) {
+                    stylishOutput.append("  ");
+                    stylishOutput.append(key);
+                    stylishOutput.append(": ");
+                    stylishOutput.append(jsonMap1.get(key));
+                    stylishOutput.append("\n");
+                } else {
+                    stylishOutput.append("- ");
+                    stylishOutput.append(key);
+                    stylishOutput.append(": ");
+                    stylishOutput.append(jsonMap1.get(key));
+                    stylishOutput.append("\n");
+                    stylishOutput.append("+ ");
+                    stylishOutput.append(key);
+                    stylishOutput.append(": ");
+                    stylishOutput.append(jsonMap2.get(key));
+                    stylishOutput.append("\n");
+                }
+            }
+        }
+        stylishOutput.append("}");
 
-//        Map<String, Object> sortedJSON1 =
-//            jsonMap1.entrySet().stream()
-//                .sorted(Entry.comparingByKey())
-//                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-//
-//        Map<String, Object> sortedJSON2 =
-//            jsonMap2.entrySet().stream()
-//                .sorted(Entry.comparingByKey())
-//                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-
-        System.out.println(sortedJSON1.toString());
-        System.out.println(sortedJSON2.toString());
-
-        return "";
+        return stylishOutput.toString();
     }
-
-
-
-
 }
