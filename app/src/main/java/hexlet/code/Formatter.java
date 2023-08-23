e5631afb9e3b1e36f677a82e602fc4467425011b
serge loie <serge.loie@yandex.ru>
2023-08-23 15:05:44 +0500

    9 шаг первый вариант plain

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -23,17 +23,42 @@ public class Formatter {
                 }
                 default -> { }
             }
-//            result.append(element.getDiffKey());
-//            result.append(": ");
-//            result.append(element.getDiffValue());
-//            result.append("\n");
         }
         result.append("}");
         return result.toString();
     }
 
+    public static String plain(List<DifferBuilder> diffLIst) {
+        StringBuilder result = new StringBuilder();
+        for (DifferBuilder element: diffLIst) {
+            result.append("Property '").append(element.getDiffKey()).append(" ' ");
+            switch (element.getStatus()) {
+                case DELETED -> result.append("was removed\n");
+                case ADDED -> {
+                    result.append("was added with value ");
+                    result.append(element.getDiffCurrentValue().getClass().isPrimitive() ? element.getDiffCurrentValue() : "[complex value]");
+                }
+                case UPDATED -> {
+                    result.append("was updated. From ");
+                    result.append(element.getDiffPreviousValue()).append(" ");
+                    result.append(element.getDiffCurrentValue()).append(" ");
+
+//                    result.append(element.getDiffPreviousValue().getClass().isPrimitive() ? element.getDiffPreviousValue() : "[complex value] ");
+//                    result.append(element.getDiffCurrentValue().getClass().isPrimitive() ? element.getDiffCurrentValue() : "[complex value]");
+                }
+                case UNCHANGED -> { }
+
+                default -> throw new IllegalStateException("Unexpected value: " + element.getStatus());
+            }
+            result.append("\n");
+
+        }
+        return result.toString();
+    }
+
     public static String buildFormattedString(String format, List<DifferBuilder> diffLIst) {
         return switch (format) {
+            case("plain") -> plain(diffLIst);
             default -> stylish(diffLIst);
         };
     }
--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -175,9 +175,25 @@ public class TestDiffer {
                   + setting3: none
                 }""";
 
+        String expectedPlain = """
+                Property 'chars2' was updated. From [complex value] to false
+                Property 'checked' was updated. From false to true
+                Property 'default' was updated. From null to [complex value]
+                Property 'id' was updated. From 45 to null
+                Property 'key1' was removed
+                Property 'key2' was added with value: 'value2'
+                Property 'numbers2' was updated. From [complex value] to [complex value]
+                Property 'numbers3' was removed
+                Property 'numbers4' was added with value: [complex value]
+                Property 'obj1' was added with value: [complex value]
+                Property 'setting1' was updated. From 'Some value' to 'Another value'
+                Property 'setting2' was updated. From 200 to 300
+                Property 'setting3' was updated. From true to 'none'""";
+
         assertEquals(expectedShort, generate("stylish", file1, file2));
         assertEquals(expectedShort, generate("stylish", file3, file4));
         assertEquals(expectedLong, generate("stylish", file11, file12));
+        assertEquals(expectedPlain, generate("plain", file11, file12));
         System.out.println(parseJSONfileToMap(file1));
     }
 
@@ -231,5 +247,13 @@ public class TestDiffer {
         System.out.println(stylish(list1));
         assertEquals(expectedLong, stylish(list1));
 
+    }
+
+    @Test
+    public void testFormattedString() {
+        String str1 = "Number %d was %s\n";
+        System.out.printf(str1, 10, "submitted");
+        System.out.printf(str1, 156, "decrised");
+        System.out.printf(str1, -15, "rejected");
     }
 }
