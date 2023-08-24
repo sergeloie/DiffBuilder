1f61e9833990517d6a00fd3156cb92115ca19c83
serge loie <serge.loie@yandex.ru>
2023-08-24 17:22:25 +0500

    10 шаг добавлен перегруженный метод generate

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -19,4 +19,15 @@ public class Differ {
         List<DifferBuilder> diffBuildList = buildDiffList(sortedList, map1, map2);
         return buildFormattedString(formatOutput, diffBuildList);
     }
+
+    public static String generate(File file1, File file2) throws IOException {
+
+        Map<String, Object> map1 = Parser.parseFileToMap(file1);
+        Map<String, Object> map2 = Parser.parseFileToMap(file2);
+        List<String> sortedList = Parser.getListOfUniqueKeys(map1, map2);
+        List<DifferBuilder> diffBuildList = buildDiffList(sortedList, map1, map2);
+        return buildFormattedString("stylish", diffBuildList);
+    }
+
+
 }
