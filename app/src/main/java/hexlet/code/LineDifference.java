5bc247438035f4a15a35a7334568764c094c6996
daBoss <serge.loie@yandex.ru>
2024-01-27 12:05:22 +0500

    gradle 8.2 -> 8.5 lombok LineDifference

--- app/src/main/java/hexlet/code/LineDifference.java
+++ app/src/main/java/hexlet/code/LineDifference.java
@@ -1,11 +1,16 @@
 package hexlet.code;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * builds a list of differences between 2 files.
  */
+@Getter
+@Setter
+@AllArgsConstructor
 public class LineDifference {
-
-
 
     public enum Status {
         DELETED,
@@ -19,99 +24,10 @@ public class LineDifference {
     private Object diffCurrentValue;
     private Object diffPreviousValue;
 
-    /**
-     * @param status
-     * @param diffKey
-     * @param diffCurrentValue
-     * @param diffPreviousValue
-     */
-    public LineDifference(Status status, String diffKey, Object diffCurrentValue, Object diffPreviousValue) {
-        this.status = status;
-        this.diffKey = diffKey;
-        this.diffCurrentValue = diffCurrentValue;
-        this.diffPreviousValue = diffPreviousValue;
-    }
-
-    /**
-     * @param status
-     * @param diffKey
-     * @param diffCurrentValue
-     */
     public LineDifference(Status status, String diffKey, Object diffCurrentValue) {
         this.status = status;
         this.diffKey = diffKey;
         this.diffCurrentValue = diffCurrentValue;
 
     }
-
-    /**
-     * @return Status [DELETED, ADDED, UNCHANGED, UPDATED].
-     */
-    public Status getStatus() {
-        return status;
-    }
-
-    /**
-     * @param status
-     */
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    /**
-     * @return Key of comparable node
-     */
-    public String getDiffKey() {
-        return diffKey;
-    }
-
-    /**
-     * @param diffKey
-     */
-    public void setDiffKey(String diffKey) {
-        this.diffKey = diffKey;
-    }
-
-    /**
-     * @return Current value of comparable node
-     */
-    public Object getDiffCurrentValue() {
-        return diffCurrentValue;
-    }
-
-    /**
-     * @param diffCurrentValue
-     */
-    public void setDiffCurrentValue(Object diffCurrentValue) {
-        this.diffCurrentValue = diffCurrentValue;
-    }
-
-    /**
-     * @return Previous value of comparable node
-     */
-    public Object getDiffPreviousValue() {
-        return diffPreviousValue;
-    }
-
-    /**
-     * @param diffPreviousValue
-     */
-    public void setDiffPreviousValue(Object diffPreviousValue) {
-        this.diffPreviousValue = diffPreviousValue;
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("DifferBuilder{");
-        sb.append("status=").append(status);
-        sb.append(", diffKey='").append(diffKey).append('\'');
-        sb.append(", diffCurrentValue=").append(diffCurrentValue);
-        sb.append(", diffPreviousValue=").append(diffPreviousValue);
-        sb.append('}');
-        return sb.toString();
-    }
-
 }
